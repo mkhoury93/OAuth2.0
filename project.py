94089249d95b2fbd844ec7938e6100d7d8a6f20b
mk <majd.khoury93@gmail.com>
2018-01-09 23:38:32 -0500

    Step 6 complete

--- project.py
+++ project.py
@@ -114,7 +114,41 @@ def gconnect():
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
+    print "logged in as %s " % login_session['username']
+    print "The user's credentials are: %s" % credentials.access_token
     return output
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a connected user
+    # if no credentials found, return message saying that user not connected
+    access_token = login_session.get('access_token')
+    print "the credentials are: %s "  % access_token
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = "https://accounts.google.com/o/oauth2/revoke?token=%s" % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        # reset user session
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps('Successfully disconnected'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        # else token was Invalid
+        response = make_response(json.dumps('Failed to revoke token for user')
+        , 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 #JSON APIs to view Restaurant Information
