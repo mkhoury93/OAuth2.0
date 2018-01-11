ea76aa3048aad9a1b5fa32b830840f67ee1090cf
mk <majd.khoury93@gmail.com>
2018-01-11 10:55:42 -0500

    Formatted comments on some methods

--- project.py
+++ project.py
@@ -32,10 +32,12 @@ session = DBSession()
 
 @app.route('/login')
 def showLogin():
-    """Here we create string of 32 characters and digits and store
+    """
+       Here we create string of 32 characters and digits and store
        it in the state of login_session, which is imported from flask.
        Afterwards, we store this in a variable called state, to which
-       we pass it along on the template 'login.html' as STATE."""
+       we pass it along on the template 'login.html' as STATE.
+    """
     state = ''.join(random.choice(string.ascii_uppercase
                     + string.digits) for x in xrange(32))
     login_session['state'] = state
@@ -44,14 +46,90 @@ def showLogin():
 
     return render_template('login.html', STATE=state)
 
+@app.route('/fbconnect')
+def fbconnect():
+    '''
+        This method handles the OAuth client of verifying a facebook
+        User's token of authorization.
+    '''
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    """This method handles the OAuth client of verifying a user's
+    '''
+        This method handles the OAuth client of verifying a user's
         token of authroization. If it's a successful verification,
-        then google plus' API pulls out the user's image, e-mail,
+        then Google Plus' API pulls out the user's image, e-mail,
         and full-name. If the user is new, it creates a new user
-        in the database!"""
+        in the database!
+    '''
 
     # Validate state token
 
@@ -168,10 +246,12 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-    """This method handles disconnecting a user from the web application
+    '''
+        This method handles disconnecting a user from the web application
         if the result of parsing his token (on the oauth2 client) is a
         200 HTTP respone, then it deletes the user's access token, google
-        plus id, username, email and picture from the login_session"""
+        plus id, username, email and picture from the login_session
+    '''
     # Only disconnect a connected user
     # if no access token found, return message saying that user not connected
 
@@ -201,9 +281,7 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-
         # else token was Invalid
-
         response = \
             make_response(json.dumps('Failed to revoke token for user'
                           ), 400)
